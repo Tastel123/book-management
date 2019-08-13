bb66686177afe560ca3e047c2a58e388765c7737
Tastel123 <244674264@qq.com>
2019-08-13 23:07:18 +0800

    1

---
+++ src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-content">
+        <Main></Main>
+        <Aside></Aside>
+        <router-view></router-view>
+        hivuiviub
+    </div>
+</template>
+
+<script>
+import Main from '@/components/common/Main'
+import Aside from '@/components/common/Aside'
+export default {
+    data() {
+        return {
+            Main,
+            Aside
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/common/Aside.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="aside-content">
+        <div class="aside-logo">
+            logo
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+.aside-content
+    width 240px
+    height 100%
+    padding-top 10px
+    -webkit-box-shadow 4px 1px 12px rgba(48,128,254,.1)
+    box-shadow 4px 1px 12px rgba(48,128,254,.1)
+
+</style>
+
